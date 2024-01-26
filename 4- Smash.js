c3245559b1113d790c2d9fa600379e974fb076db
AlexanderDev2004 <alexgaming2120@gmail.com>
2024-01-26 21:44:54 +0700

    fork : 4-Smash

---
+++ 4- Smash.js
@@ -0,0 +1,9 @@
+// 4/336
+// function smash (words) {
+//     return words.join(" ");
+//  };
+
+
+ const smash = (words) => words.join(' ');
+ 
+ console.log(smash(["this", "is", "a", "really", "long", "sentence"]));
